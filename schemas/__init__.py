e6346d8fd5a65393099108ca209f7c861d92d412
Bruno Bastos <pinheirobastos@gmail.com>
2024-09-03 22:49:10 -0300

    feat: BuscaReviewSchema

--- schemas/__init__.py
+++ schemas/__init__.py
@@ -1,4 +1,4 @@
-from schemas.review_schema import ReviewSchema,  ReviewViewSchema, ReviewDelSchema, ListaReviewsSchema, \
+from schemas.review_schema import ReviewSchema,  ReviewViewSchema, ReviewDelSchema, ListaReviewsSchema, BuscaReviewSchema, \
                                     apresenta_review, apresenta_reviews
                                         
 from schemas.error_schema import ErrorSchema
